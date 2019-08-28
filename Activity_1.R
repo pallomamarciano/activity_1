dad65ac97cd6d6b79f00d23c3d90a160c9189e8d
Palloma Marciano <lloma.m@gmail.com>
2019-08-27 21:35:06 -0300

    First

---
+++ Activity_1.R
@@ -0,0 +1,24 @@
+#Atividade 1 - R for Data Science
+
+#1. Introduction
+
+## Instalando pacotes
+install.packages(c("nycflights13", "gapminder", "Lahman"))
+
+1 + 2
+
+## Updating a package
+install.packages("tidyverse")
+tidyverse_update() #Error =  could not find function "tidyverse_update"
+
+
+# 2. Introduction
+
+# 3. Data visualization
+
+## Access to the data
+library(tidyverse)
+
+mpg
+
+## Ploting
