09a0272d81062e647243b22b281c86547d54bcc4
Palloma Marciano <lloma.m@gmail.com>
2019-09-07 16:56:14 -0300

    First

--- atividade-1-palloma-marciano.R
+++ atividade-1-palloma-marciano.R
@@ -1,4 +1,4 @@
-'''
+'
 File-Name: atividade-1-palloma-marciano.R
 Data: 2019, Ago 27
 Author: Marciano, Palloma
@@ -10,7 +10,7 @@ Data Used: mpg
   **ATIVIDADE 1 - R FOR DATA SCIENCE **
   *************************************
   
-'''
+'
 
 install.packages("lintr")
 library(lintr)
@@ -553,3 +553,45 @@ arrange(flights, (air_time))
 arrange(flights, (distance))
 
 arrange(flights, desc(distance))
+
+
+'5.4.1 Exercises'
+
+'1. Brainstorm as many ways as possible to select dep_time, dep_delay, 
+arr_time, and arr_delay from flights.'
+
+library(nycflights13)
+
+flights
+
+select(flights, dep_time, dep_delay, arr_time, arr_delay)
+
+select(flights, dep_time:arr_delay) # Don't match
+
+'2. What happens if you include the name of a variable multiple times in 
+a select() call?'
+
+
+
+'3. What does the one_of() function do? Why might it be helpful in 
+conjunction with this vector?
+
+vars <- c("year", "month", "day", "dep_delay", "arr_delay")'
+
+'4. Does the result of running the following code surprise you? How do 
+the select helpers deal with case by default? How can you change that 
+default?
+
+select(flights, contains("TIME"))'
+
+
+
+
+
+
+
+
+
+
+
+
